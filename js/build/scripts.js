cf4feac37484be69d3eaab3ba6421c1a5237707d
root <root@debian>
2016-04-03 16:11:28 +0000

    basic theme foundation

---
+++ js/build/scripts.js
@@ -0,0 +1,2 @@
+!function($,Drupal,window,document){Drupal.behaviors.basic={attach:function(context,settings){$(window).load(function(){}),$(window).resize(function(){}),$(window).scroll(function(){}),$(document).ready(function(){})}}}(jQuery,Drupal,this,this.document);
+//# sourceMappingURL=scripts.map
